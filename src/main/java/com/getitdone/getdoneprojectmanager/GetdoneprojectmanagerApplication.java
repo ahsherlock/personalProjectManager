c6b9b3819da3fd98a0d04d06dbe6f6c1c28c8031
Alec Sherlock <ahsherlock0@gmail.com>
2020-02-19 22:29:43 -0800

    first commit

---
+++ src/main/java/com/getitdone/getdoneprojectmanager/GetdoneprojectmanagerApplication.java
@@ -0,0 +1,13 @@
+package com.getitdone.getdoneprojectmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GetdoneprojectmanagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GetdoneprojectmanagerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/getitdone/getdoneprojectmanager/GetdoneprojectmanagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.getitdone.getdoneprojectmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GetdoneprojectmanagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
