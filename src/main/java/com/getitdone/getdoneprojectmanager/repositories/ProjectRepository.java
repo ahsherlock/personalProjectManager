9e4ffc84d1ea498929298980e2e11136032c7c7b
Alec Sherlock <ahsherlock0@gmail.com>
2020-02-19 23:21:00 -0800

    added project object and project repositories

---
+++ src/main/java/com/getitdone/getdoneprojectmanager/repositories/ProjectRepository.java
@@ -0,0 +1,14 @@
+package com.getitdone.getdoneprojectmanager.repositories;
+
+
+import com.getitdone.getdoneprojectmanager.domain.Project;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProjectRepository extends CrudRepository<Project, Long> {
+
+    @Override
+    Iterable<Project> findAllById(Iterable<Long> iterable);
+
+}
